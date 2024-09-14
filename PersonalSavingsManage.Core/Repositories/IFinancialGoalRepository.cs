39fec1527d9b5e5b065fa8eec5dd85eb7a4d2aff
RobertoFarias1989 <robertosf1989@gmail.com>
2024-09-14 17:34:14 -0300

    feature: implemented core layer;

--- PersonalSavingsManage.Core/Repositories/IFinancialGoalRepository.cs
+++ PersonalSavingsManage.Core/Repositories/IFinancialGoalRepository.cs
@@ -4,8 +4,8 @@ namespace PersonalSavingsManage.Core.Repositories;
 
 public interface IFinancialGoalRepository
 {
-    public Task<List<FinancialGoal>> GetAllAsync();
-    public Task<FinancialGoal> GetByIdAsync(string id);
-    public Task<FinancialGoal> GetDetailsByIdAsync(string id);
-    public Task AddAsync(FinancialGoal goal);
+    Task<List<FinancialGoal>> GetAllAsync();
+    Task<FinancialGoal> GetByIdAsync(string id);
+    Task<FinancialGoal> GetDetailsByIdAsync(string id);
+    Task AddAsync(FinancialGoal goal);
 }
