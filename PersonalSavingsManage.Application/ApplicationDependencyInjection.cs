5b8d2be7f41a12cfd58eee691b1c1a7320685dee
Roberto_de_Souza_Farias <farias.sistemas@lar.ind.br>
2025-12-27 16:30:19 -0300

    feature: adição do projeto api, criação da classe estática para a camada application;

---
+++ PersonalSavingsManage.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalSavingsManage.Application.FinancialGoal.Commands.CreateFinancialGoal;
+using System.Reflection;
+
+namespace PersonalSavingsManage.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        services.AddMediator()
+            .AddValidator();
+
+        return services;
+    }
+
+    public static IServiceCollection AddMediator( this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(CreateGoalCommand));
+
+        return services;
+    }
+
+    public static IServiceCollection AddValidator(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddFluentValidationAutoValidation();
+
+        return services;
+    }
+}
